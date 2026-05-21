30509a1f5090fc0385d17502123216bcb3df7157
Clayton Prebelli <clayton.prebelli.act@gmail.com>
2026-05-20 21:49:44 -0300

    app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.agendaclayton.app',
+  appName: 'AgendaClayton',
+  webDir: 'dist/agenda-clayton/browser'
+};
+
+export default config;
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch()), provideIonicAngular({}),
   ],
 };
